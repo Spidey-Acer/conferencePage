f26eee03c94bd7ac2535f700accf7e31fad70518
Peter-Kibet <peter.kibet@hotmail.com>
2023-05-22 07:16:57 +0300

    Add home section and styling for mobile

--- script.js
+++ script.js
@@ -3,19 +3,27 @@ const closeButton = document.querySelector(".close-button");
 const mobileMenu = document.querySelector(".mobile-menu");
 const mobileMenuItem = document.querySelectorAll(".mobile-menu-item");
 
-function toggleMobileMenu() {
+function showMobileMenu() {
   mobileMenu.style.display = "flex";
 }
 
-function closeMobileMenu() {
+function hideMobileMenu() {
   mobileMenu.style.display = "none";
+}
+
+function toggleMobileMenu() {
+  if (mobileMenu.style.display === "none") {
+    showMobileMenu();
+  } else {
+    hideMobileMenu();
+  }
 }
 
 function handleMobileMenuItemClick(event) {
   const { target } = event;
   const sectionId = target.getAttribute("href").substring(1);
 
-  closeMobileMenu();
+  hideMobileMenu();
 
   document.querySelector(sectionId).scrollIntoView({
     behavior: "smooth",
@@ -23,8 +31,11 @@ function handleMobileMenuItemClick(event) {
   });
 }
 
+// Hide the mobile menu by default
+hideMobileMenu();
+
 menuButton.addEventListener("click", toggleMobileMenu);
-closeButton.addEventListener("click", closeMobileMenu);
+closeButton.addEventListener("click", hideMobileMenu);
 
 mobileMenuItem.forEach((item) => {
   item.addEventListener("click", handleMobileMenuItemClick);
