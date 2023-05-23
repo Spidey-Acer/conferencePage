00d616fa036590a6e16bf9d0aed0ab1657c197dc
Peter-Kibet <peter.kibet@hotmail.com>
2023-05-24 02:40:49 +0300

    Add one nav for all the pages

---
+++ pages/navBar.js
@@ -0,0 +1,54 @@
+const menuButton = document.querySelector(".menu-button");
+const closeButton = document.querySelector(".close-button");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuItem = document.querySelectorAll(".mobile-menu-item");
+
+function showMobileMenu() {
+  mobileMenu.style.display = "flex";
+}
+
+function hideMobileMenu() {
+  mobileMenu.style.display = "none";
+}
+
+function toggleMobileMenu() {
+  if (mobileMenu.style.display === "none") {
+    showMobileMenu();
+  } else {
+    hideMobileMenu();
+  }
+}
+
+function handleMobileMenuItemClick(event) {
+  const { target } = event;
+  const sectionId = target.getAttribute("href").substring(1);
+
+  hideMobileMenu();
+
+  document.querySelector(sectionId).scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
+
+// Hide the mobile menu by default
+hideMobileMenu();
+
+menuButton.addEventListener("click", toggleMobileMenu);
+closeButton.addEventListener("click", hideMobileMenu);
+
+mobileMenuItem.forEach((item) => {
+  item.addEventListener("click", handleMobileMenuItemClick);
+});
+
+// sticky mobile menu
+
+const stickyMobileMenu = document.querySelector(".main-nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    stickyMobileMenu.classList.add("sticky");
+  } else {
+    stickyMobileMenu.classList.remove("sticky");
+  }
+});
